dbaeb5e3ceee2967c03ae108554b999284da0333
MartinPerezDev <martinperez9713@gmail.com>
2024-05-01 18:02:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Como usar
+
+npm install
+
+npm start
+
+### Ruta de productos
+
+`/api/productos`
+
+ejemplo:
+
+`http://localhost:8080/api/productos`
---
+++ src/data/products.js
@@ -0,0 +1,31 @@
+[
+  {
+    "id": 2,
+    "nombre": "Bioshock Infinite",
+    "descripcion": "Description 2",
+    "categoria": "survival",
+    "stock": 1,
+    "imagen": "/image/bioshock-infinite.jpg",
+    "timestamp": "26/03/2024 20:48"
+  },
+  {
+    "id": 3,
+    "nombre": "Need for speed",
+    "descripcion": "descripcion 3",
+    "precio": 1200,
+    "stock": 1,
+    "categoria": "racing",
+    "imagen": "/image/black-4-blood.jpg",
+    "timestamp": "29/04/2024 22:18"
+  },
+  {
+    "id": 4,
+    "nombre": "Black 4 blood",
+    "descripcion": "descripcion 3",
+    "precio": 250,
+    "stock": 1,
+    "categoria": "shooter",
+    "imagen": "/image/black-4-blood.jpg",
+    "timestamp": "29/04/2024 22:24"
+  }
+]
